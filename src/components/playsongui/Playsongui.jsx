ec5be7bb5615a8eb5bc1fe80a06838e20858701f
soumyaranjan-dev <ranjansoumya33479@gmail.com>
2025-09-02 19:51:17 +0530

    final commit

--- src/components/playsongui/Playsongui.jsx
+++ src/components/playsongui/Playsongui.jsx
@@ -1,29 +1,23 @@
-import React, { useState } from "react";
-import { useLocation } from "react-router-dom";
+import React, { useRef, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Playsongui = () => {
-  const [playsong, setplaysong] = useState(false);
+  const navigate = useNavigate();
   const { state } = useLocation();
   const song = state?.song;
 
-  console.log(playsong);
   return (
-    <section className="backdrop-blur-sm h-[calc(100vh-4rem)] overflow-y-scroll customScrollbar w-full p-2 md:p-4 lg:p-6 bg-transparent rounded-md pt-[6rem] lg:pt-[5rem] md:pt-[5rem] pb-[4rem] flex flex-col justify-between gap-5 duration-700 space-y-2 text-sm">
+    <section className="backdrop-blur-sm h-screen overflow-y-scroll customScrollbar w-full p-2 md:p-4 lg:p-6 bg-transparent rounded-md pt-[6rem] lg:pt-[5rem] md:pt-[5rem] pb-[4rem] flex flex-col justify-center items-center gap-5 duration-700 space-y-2 text-sm">
       {song ? (
-        <>
-          <h1>PlaySongUI</h1>
-          <button
-            onClick={() => {
-              setplaysong(!playsong);
-            }}
-          >
-            play
-          </button>
-          <audio src={song.cloudinaryUrl} controls autoPlay={playsong}></audio>
-        </>
+        <section className="h-[20rem] w-[20rem] bg-black rounded-lg flex flex-col justify-center items-center space-y-2 p-2">
+          <img src={song.cover} alt="" width={100} height={100} />
+          <h1>{song.title}</h1>
+          <audio src={song.cloudinaryUrl} controls autoPlay></audio>
+        </section>
       ) : (
-        <h1>no song selected</h1>
+        <h1>no song selected ...</h1>
       )}
+      <p onClick={() => navigate("/dashboard")}>back</p>
     </section>
   );
 };
